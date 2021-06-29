57ccc3d92f2056358e0dd108cb19401aa646b593
Nathan Wilson <natewilson135@gmail.com>
2021-06-29 10:52:11 -0400

    reverted insurance verification to post full width hero. Kept + version

--- insurance-verification.php
+++ insurance-verification.php
@@ -18,28 +18,67 @@ sbh_center()->print_styles('sbh_center-custom-page-nosidebar', 'sbh_center-conte
 
 ?>
 
+<?php if (get_field('has_google_translate')) : ?>
+    <div id="google_translate_element"></div>
+    <script>
+        function googleTranslateElementInit() {
 
-<main id="primary" class="insurance_verification_page">
-    <?php get_template_part('template-parts/heros/desktop_and_mobile_hero_full_width'); ?>
+            new google.translate.TranslateElement({
 
-    <div class="content_container">
-        <div class="page_wrapper">
-            <!-- Page Content -->
-            <section class="page_content">
-                <?php get_template_part('template-parts/content/googleTranslate'); ?>
-                <?php echo get_field('content') ?>
-            </section>
+                pageLanguage: 'en'
 
-            <section class="insurance_form_section">
-                <div class="insurance_form_wrapper">
-                    <?php $insuranceVerificationGForm = '[gravityform id="' . get_field('insurance_verification_form_id', 'option') . '" title="false" description="false"]';
-                    echo do_shortcode($insuranceVerificationGForm); ?>
-                </div>
-            </section>
+            }, 'google_translate_element');
 
-            <?php get_template_part('template-parts/content/pageDisclaimer'); ?>
+        }
+    </script>
+    <script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+<?php endif; ?>
+
+<main id="primary" class="maxWidth insurance_verification_page">
+    <div class="page_image">
+        <img class="page_desktop_image hero_image hide_on_mobile show_on_tablet" src="<?php echo get_field('desktop_image')['url']; ?>">
+        <img class="page_mobile_image hero_image hide_on_desktop hide_on_tablet" src="<?php echo get_field('mobile_image')['url']; ?>">
+        <div class="page_mobile_image_text_and_cta_container">
+            <h1><?php echo get_field('insurance_page_headline') ?></h1>
+            <p class="hero_subheadline"><?php echo get_field('insurance_page_subheadline_paragraph') ?></p>
         </div>
     </div>
-</main>
-<?php
-get_footer();
+    <div class="hide_on_mobile">
+        <h1><?php echo get_field('insurance_page_headline') ?></h1>
+        <p class="hero_subheadline"><?php echo get_field('insurance_page_subheadline_paragraph') ?></p>
+    </div>
+
+    <!-- Covid Notice -->
+    <section class="covid_section hide_on_desktop">
+        <a href="/our-response-to-the-corona-virus-health-concern/">Learn about Our Covid response</a>
+    </section>
+
+    <div class="insurance_page_container">
+
+        <section class="insurance_page_video_content_section">
+            <div class="insurance_page_video_content_container">
+                <div class="insurance_page_video_headline">
+                    <h3><?php echo get_field('video_headline') ?></h3>
+                </div>
+                <div class="insurance_page_video_iframe">
+                    <?php echo get_field('youtube_iframe') ?>
+                </div>
+                <div class="insurance_page_video_content_container">
+                    <?php echo get_field('video_paragraph') ?>
+                </div>
+            </div>
+        </section>
+
+        <section class="insurance_page_form_section">
+            <div class="insurance_page_form_container">
+                <?php $insuranceVerificationGForm = '[gravityform id="' . get_field('insurance_verification_form_id', 'option') . '" title="false" description="false"]';
+                echo do_shortcode($insuranceVerificationGForm); ?>
+            </div>
+        </section>
+
+    </div>
+    <section class="page_disclosure_section">
+        <?php get_template_part('template-parts/content/pageDisclaimer'); ?>
+    </section>
+</main> <?php
+        get_footer();
