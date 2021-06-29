fe37769d5bbd8637328ec2b0b83308bbb241d5e1
Nathan Wilson <natewilson135@gmail.com>
2021-06-29 14:05:12 -0400

    removed + from templates

--- staff.php
+++ staff.php
@@ -24,27 +24,23 @@ get_template_part('template-parts/javascript/navigationJs');
 
 
 <main id="primary" class="maxWidth">
+	<?php get_template_part('template-parts/heros/desktop_and_mobile_hero_full_width'); ?>
 
-	<div class="custom_page_noSideBar_wrapper">
-		<div class="page_image">
-			<img class="page_desktop_image hero_image hide_on_mobile show_on_tablet" src="<?php echo get_field('desktop_image')['url']; ?>">
-			<img class="page_mobile_image hero_image hide_on_desktop hide_on_tablet" src="<?php echo get_field('mobile_image')['url']; ?>">
-			<div class="page_mobile_image_text_and_cta_container">
-				<h1><?php echo get_field('page_headline') ?></h1>
-				<p class="hero_subheadline"><?php echo get_field('page_subheadline_paragraph') ?></p>
-			</div>
+	<div class="content_container">
+		<div class="page_wrapper">
+			<!-- Page Content -->
+			<section class="page_content list_styling">
+				<section class="staff_page_content">
+					<?php echo get_field('page_content'); ?>
+				</section>
+
+				<?php get_template_part('template-parts/content/universal_staff'); ?>
+			</section>
+
+			<?php get_template_part('template-parts/content/pageDisclaimer'); ?>
+
+			<?php get_template_part('template-parts/content/phoneTreatmentCta'); ?>
 		</div>
-		<div class="hide_on_mobile">
-			<h1><?php echo get_field('page_headline') ?></h1>
-			<p class="hero_subheadline"><?php echo get_field('page_subheadline_paragraph') ?></p>
-		</div>
-		<section class="staff_page_content">
-			<?php echo get_field('page_content'); ?>
-		</section>
-
-		<?php get_template_part('template-parts/content/universal_staff'); ?>
-
-		<?php get_template_part('template-parts/content/pageDisclaimer'); ?>
 	</div>
 </main>
 <?php
