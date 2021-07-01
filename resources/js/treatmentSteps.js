3b33128f8a7c0f9a079545b51cf5354703d39946
Nathan Wilson <natewilson135@gmail.com>
2021-07-01 13:21:57 -0400

    added ../

--- resources/js/treatmentSteps.js
+++ resources/js/treatmentSteps.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function ($) {
     $('.step').each(function (index, element) {
         // element == this
         $(element).not('.active').addClass('done');
-        $('.done').html('<img src="../icons/checkmark_icon_white.png" >');
+        $('.done').html('<img src="../../icons/checkmark_icon_white.png" >');
         if ($(this).is('.active')) {
             return false;
         }
