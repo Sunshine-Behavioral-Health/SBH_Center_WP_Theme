42e9c45570002e5cab421d2df0b43534621621f0
Nathan Wilson <natewilson135@gmail.com>
2021-07-01 11:29:18 -0400

    treatment step updates

--- template-parts/content/treatment_step_progress_bar.php
+++ template-parts/content/treatment_step_progress_bar.php
@@ -2,12 +2,12 @@
     <div class="treatment_step_progress_bar_container">
         <div id="steps">
             <?php
-            if (have_rows('', 'option')) :
+            if (have_rows('treatment_steps', 'option')) :
                 $treatmentStepNumber = 0;
-                while (have_rows('', 'option')) : the_row();
+                while (have_rows('treatment_steps', 'option')) : the_row();
                     $treatmentStepNumber++
             ?>
-                    <div class="step" data-desc="Listing information"><?php echo $treatmentStepNumber ?></div>
+                    <div class="step" data-desc="<?php echo get_sub_field('step_name', 'option') ?>"><?php echo $treatmentStepNumber ?></div>
             <?php
                 endwhile;
             endif;
--- template-parts/heros/desktop_and_mobile_hero_full_width.php
+++ template-parts/heros/desktop_and_mobile_hero_full_width.php
@@ -1,8 +1,14 @@
 <?php $centerValue = get_field('center'); ?>
 
 <div class="desktop_and_mobile_hero_image_wrapper">
-    <img class="page_desktop_image hero_image hide_on_mobile show_on_tablet" src="<?php echo get_field('desktop_image')['url']; ?>">
-    <img class="page_mobile_image hero_image hide_on_desktop hide_on_tablet" src="<?php echo get_field('mobile_image')['url']; ?>">
+    <?php if (get_field('desktop_image')) : ?>
+        <img class="page_desktop_image hero_image hide_on_mobile show_on_tablet" src="<?php echo get_field('desktop_image')['url']; ?>">
+    <?php endif; ?>
+
+    <?php if (get_field('mobile_image')) : ?>
+        <img class="page_mobile_image hero_image hide_on_desktop hide_on_tablet" src="<?php echo get_field('mobile_image')['url']; ?>">
+    <?php endif; ?>
+
     <div class="hero_banner_headlines_container">
         <div class="hero_banner_headlines_wrapper">
             <?php if ($centerValue == "Chapters Capistrano") : ?>
