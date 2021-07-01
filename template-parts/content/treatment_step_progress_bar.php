e5c694210a50719f7efcd958cfc92dfd09b5ef55
Nathan Wilson <natewilson135@gmail.com>
2021-07-01 11:32:10 -0400

    treatment step updates

--- template-parts/content/treatment_step_progress_bar.php
+++ template-parts/content/treatment_step_progress_bar.php
@@ -1,17 +1,20 @@
 <section class="treatment_step_progress_bar_section">
     <div class="treatment_step_progress_bar_container">
         <div id="steps">
+            before treatment steps progress bar repeater
             <?php
             if (have_rows('treatment_steps', 'option')) :
                 $treatmentStepNumber = 0;
                 while (have_rows('treatment_steps', 'option')) : the_row();
                     $treatmentStepNumber++
             ?>
+                    <p>inside treatment steps progress bar repeater</p>
                     <div class="step" data-desc="<?php echo get_sub_field('step_name', 'option') ?>"><?php echo $treatmentStepNumber ?></div>
             <?php
                 endwhile;
             endif;
             ?>
+            after treatment steps progress bar repeater
         </div>
     </div>
 </section>
