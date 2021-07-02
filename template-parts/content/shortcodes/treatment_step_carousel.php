9337bc31f22f6a7d3d34987290241a7000377642
Nathan Wilson <natewilson135@gmail.com>
2021-07-02 10:59:43 -0400

    treatment_step_carousel scss

--- template-parts/content/shortcodes/treatment_step_carousel.php
+++ template-parts/content/shortcodes/treatment_step_carousel.php
@@ -9,10 +9,10 @@
             ?>
                 <div class="treatment_step_carousel_element">
                     <div class="treatment_step_carousel_element_content">
-                        <h3><?php echo get_sub_field('step_headline', 'option') ?></h3>
                         <h4><?php echo get_sub_field('step_subheadline', 'option') ?></h4>
+                        <h3><?php echo get_sub_field('step_headline', 'option') ?></h3>
                         <p><?php echo get_sub_field('step_paragraph', 'option') ?></p>
-                        <a href="<?php echo get_sub_field('step_url', 'option') ?>"><?php get_sub_field('step_cta_text', 'option') ?><img src="<?php echo get_template_directory_uri() . '/icons/right_arrow.png' ?>" alt=""></a>
+                        <a href="<?php echo get_sub_field('step_url', 'option') ?>"><?php echo get_sub_field('step_cta_text', 'option') ?><img src="<?php echo get_template_directory_uri() . '/icons/right_arrow.png' ?>" alt=""></a>
                     </div>
                     <div class="treatment_step_carousel_element_image">
                         <img src="<?php echo get_sub_field('step_image', 'option')['url'] ?>" alt="">
