9c306886b88432b3785653dc7c759584aff46802
Nathan Wilson <natewilson135@gmail.com>
2021-07-06 13:22:59 -0400

    more treatment carousel changes

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ jQuery(document).ready(function ($) {
     let slides;
     showSlides();
 
-    $(".treatment_step_last").on('click', plusSlides(-1));
-    $(".treatment_step_next").on('click', plusSlides(1));
+    $(".prev").on('click', plusSlides(-1));
+    $(".next").on('click', plusSlides(1));
 
     function showSlides() {
         console.log("top of showSlides")
