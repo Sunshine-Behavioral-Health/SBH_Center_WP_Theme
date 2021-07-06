2db4e9ca4529a506c1f52b1703b7a4e34beb4602
Nathan Wilson <natewilson135@gmail.com>
2021-07-06 10:12:18 -0400

    treatment step carousel progress bar changes

--- template-parts/content/shortcodes/treatment_step_carousel.php
+++ template-parts/content/shortcodes/treatment_step_carousel.php
@@ -42,10 +42,9 @@
                     </ul>
                 </div>
                 <div class="glide__arrows treatment_step_carousel_arrows" data-glide-el="controls">
-
                     <div class="circular">
                         <div class="inner"></div>
-                        <div class="number">next</div>
+                        <div class="number">left</div>
                         <div class="circle">
                             <div class="bar left">
                                 <div class="progress"></div>
@@ -56,7 +55,20 @@
                         </div>
                     </div>
 
-                    <button class="glide__arrow glide__arrow--left" data-glide-dir=">">back</button>
+                    <div class="circular">
+                        <div class="inner"></div>
+                        <div class="number">right</div>
+                        <div class="circle">
+                            <div class="bar left">
+                                <div class="progress"></div>
+                            </div>
+                            <div class="bar right">
+                                <div class="progress"></div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <button class="glide__arrow glide__arrow--left" data-glide-dir="<">last</button>
                     <button class="glide__arrow glide__arrow--right" data-glide-dir=">">next</button>
                 </div>
             </div>
