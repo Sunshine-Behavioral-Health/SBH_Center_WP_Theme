222ae1269f8087a88ee2050358ebff3d735a0287
Nathan Wilson <natewilson135@gmail.com>
2021-07-06 13:59:59 -0400

    as

--- template-parts/content/shortcodes/treatment_step_carousel.php
+++ template-parts/content/shortcodes/treatment_step_carousel.php
@@ -20,7 +20,7 @@
                     </div>
                 <?php endwhile; ?>
 
-                <div class="treatment_step_carousel_buttons prev">
+                <div class="treatment_step_carousel_buttons prev" onclick="(plusSlides(-1))">
                     <div class="treatment_step_carousel_button">
                         <svg style="max-height:100px;" version="1.1" id="circle" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" xml:space="preserve">
                             <circle cx="50" cy="50" r="40" stroke="grey" stroke-width="3" fill="none" />
@@ -28,7 +28,7 @@
                         <a class="prev_arrow">&#10094;</a>
 
                     </div>
-                    <div class="treatment_step_carousel_button next">
+                    <div class="treatment_step_carousel_button next" onclick="(plusSlides(1))">
                         <svg style="max-height:100px;" version="1.1" id="circle" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" xml:space="preserve">
                             <circle fill="none" stroke="#000" stroke-width="4" stroke-mitterlimit="0" cx="50" cy="50" r="48" stroke-dasharray="360" stroke-linecap="round" transform="rotate(-90 ) translate(-100 0)">
                                 <animate attributeName="stroke-dashoffset" values="360;0" dur="7s" repeatCount="indefinite"></animate>
@@ -49,14 +49,9 @@
     let slideIndex = 0;
     let slides;
 
-    const lastButton = document.getElementsByClassName('last');
-    const nextButton = document.getElementsByClassName('next');
-
 
     showSlides();
 
-    lastButton.addEventListener("click", plusSlides(-1), false);
-    nextButton.addEventListener("click", plusSlides(1), false);
 
     function showSlides() {
         var i;
