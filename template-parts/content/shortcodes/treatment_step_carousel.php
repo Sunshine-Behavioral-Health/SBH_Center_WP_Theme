c548209589b80aee2c523092895f1b32b0588808
Nathan Wilson <natewilson135@gmail.com>
2021-07-06 13:47:06 -0400

    moved function outside of jQuery scope

--- template-parts/content/shortcodes/treatment_step_carousel.php
+++ template-parts/content/shortcodes/treatment_step_carousel.php
@@ -53,6 +53,7 @@
 
         $(".prev").on('click', () => {
             plusSlides(-1)
+            console.log("clicked prev")
         });
         // $(".next").on('click', () => {
         //     console.log("clicked next button")
@@ -77,20 +78,22 @@
             setTimeout(showSlides, 7000);
         }
 
-        function plusSlides(position) {
-            console.log("inside plusSlides function")
-            slideIndex += position;
-            if (slideIndex > slides.length) {
-                slideIndex = 1
-            } else if (slideIndex < 1) {
-                slideIndex = slides.length
-            }
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
-            slides[slideIndex - 1].style.display = "flex";
 
-            console.log("end of plus slides function")
-        }
     });
+
+    function plusSlides(position) {
+        console.log("inside plusSlides function")
+        slideIndex += position;
+        if (slideIndex > slides.length) {
+            slideIndex = 1
+        } else if (slideIndex < 1) {
+            slideIndex = slides.length
+        }
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slides[slideIndex - 1].style.display = "flex";
+
+        console.log("end of plus slides function")
+    }
 </script>
