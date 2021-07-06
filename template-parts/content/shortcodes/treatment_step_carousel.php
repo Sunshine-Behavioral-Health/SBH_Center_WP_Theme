482accb6667002799d23fbeef919bb340f61b8a3
Nathan Wilson <natewilson135@gmail.com>
2021-07-06 15:09:34 -0400

    auto carousel broke it but foward/back buttons confirmed work

--- template-parts/content/shortcodes/treatment_step_carousel.php
+++ template-parts/content/shortcodes/treatment_step_carousel.php
@@ -44,8 +44,8 @@
 </section>
 
 <script>
-    var slideIndex = 0;
-    showSlides();
+    var slideIndex = 1;
+    showSlides(slideIndex);
 
     // Next/previous controls
     function plusSlides(n) {
@@ -57,19 +57,19 @@
         showSlides(slideIndex = n);
     }
 
-
-    function showSlides() {
+    function showSlides(n) {
         let i;
         const slides = document.getElementsByClassName("treatment_step_carousel_element");
+        if (n > slides.length) {
+            slideIndex = 1
+        }
+        if (n < 1) {
+            slideIndex = slides.length
+        }
         for (i = 0; i < slides.length; i++) {
             slides[i].style.display = "none";
         }
-        slideIndex++;
-        if (slideIndex > slides.length) {
-            slideIndex = 1
-        }
-        slides[slideIndex - 1].style.display = "flex";
-        setTimeout(showSlides, 7000); // Change image every 2 seconds
+        slides[slideIndex - 1].style.display = "block";
     }
 
     function reset_animation() {
