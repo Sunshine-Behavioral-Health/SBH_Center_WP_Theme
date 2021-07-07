4e383281808febf1a1fa5b0771b91afe176a1bcf
Nathan Wilson <natewilson135@gmail.com>
2021-07-07 12:53:58 -0400

    removed console.logs from treatment_step_progress_bar. Confirmed success of adding javascript with ob_start() and ob_get_clean();

--- template-parts/javascript/treatmentStepsJS.php
+++ template-parts/javascript/treatmentStepsJS.php
@@ -1,7 +1,5 @@
 <script>
-    console.log("inside treatmentStep JS outside of jquery")
     jQuery(document).ready(function($) {
-        console.log("inside treatment step Js at top of ready")
         $('.step').each(function(index, element) {
             // element == this
             $(element).not('.active').addClass('done');
@@ -9,7 +7,6 @@
             if ($(this).is('.active')) {
                 return false;
             }
-            console.log("inside treatmentStep JS main function")
         });
 
         var fixmeTop = $('.treatment_step_progress_bar_section').offset().top; // get initial position of the element
@@ -24,12 +21,10 @@
                     top: '150px',
                     left: '0'
                 });
-                console.log("added fix position")
             } else { // apply position: static
                 $('.treatment_step_progress_bar_section').css({ // if you scroll above it
                     position: 'static'
                 });
-                console.log("else position")
             }
 
         });
